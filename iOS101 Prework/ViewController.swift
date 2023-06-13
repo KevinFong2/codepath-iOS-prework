452e402677d82600de671fd7c39a1da6f67dd6d9
Kevin Fong <kevinfong21@gmail.com>
2023-06-13 19:08:04 -0400

    changed the font of the labels

--- iOS101 Prework/ViewController.swift
+++ iOS101 Prework/ViewController.swift
@@ -19,15 +19,13 @@ class ViewController: UIViewController {
         view.backgroundColor = randomColor
     }
     
-    
-    
     func changeColor() -> UIColor{
-        let red = CGFloat.random(in:0...1)
-        let green = CGFloat.random(in:0...1)
-        let blue = CGFloat.random(in:0...1)
-        
-        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
-    }
-    
+            let red = CGFloat.random(in:0...1)
+            let green = CGFloat.random(in:0...1)
+            let blue = CGFloat.random(in:0...1)
+
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+
 }
 
